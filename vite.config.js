9d747798ae317febf1987d1f83dcf759fa514ed2
agusdwyna <dwiyanaikadek@gmail.con>
2026-03-29 10:34:07 +0800

    p

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    
   ],
-    base: '/surflessontamoco/'
+
+  base: import.meta.env.VITE_BASE_PATH || '/surflessontamoco/',
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
